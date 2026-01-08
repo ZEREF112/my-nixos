9e1432f608de75b303085cd4ea41403c681cd2b1
ZEREF112 <94997978+ZEREF112@users.noreply.github.com>
2026-01-08 19:08:51 +0300

    Update NIX With HM2

--- home.nix
+++ home.nix
@@ -8,9 +8,20 @@
   home.stateVersion = "24.05";
   programs.home-manager.enable = true;
 
+  wayland.windowManager.hyprland = {
+    enable = true;
+    xwayland.enable = true;
+    systemd.enable = true;
+  };
   # Run Hyprland With HM
   wayland.windowManager.hyprland.enable = true;
 
   # Hyprland Config
   xdg.configFile."hypr".source = ./hypr;
+
+  xdg.portal = {
+    enable = true;
+    extraPortals = [ pkgs.xdg-desktop-portal-hyprland ];
+    config.common.default = "*";
+  };
 }
